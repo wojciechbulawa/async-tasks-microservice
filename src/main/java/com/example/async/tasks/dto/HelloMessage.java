5d639e407f00991f045464803899519f4ad7a42a
wojciechbulawa <bulawa.wojciech.firma@gmail.com>
2023-02-19 14:46:30 +0100

    Add json rest responses

---
+++ src/main/java/com/example/async/tasks/dto/HelloMessage.java
@@ -0,0 +1,13 @@
+package com.example.async.tasks.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloMessage {
+
+    private String message;
+}
--- src/main/java/com/example/async/tasks/rest/HelloResource.java
+++ src/main/java/com/example/async/tasks/rest/HelloResource.java
@@ -1,20 +1,23 @@
 package com.example.async.tasks.rest;
 
+import com.example.async.tasks.dto.HelloMessage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/api/hello")
 public class HelloResource {
 
     @GetMapping("/non-logged-in")
-    public String hello() {
-        return "Hello";
+    public HelloMessage hello() {
+        return new HelloMessage("Hello");
     }
 
-    @GetMapping("/logged-in")
-    public String helloLoggedIn() {
-        return "Hello logged-in user";
+    @GetMapping(path = "/logged-in", produces = APPLICATION_JSON_VALUE)
+    public HelloMessage helloLoggedIn() {
+        return new HelloMessage("Hello logged-in user");
     }
 }
